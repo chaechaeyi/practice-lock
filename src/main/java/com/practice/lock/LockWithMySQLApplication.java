be3202cb3712e14ed5dcc8dddabe231fc068aac0
chaechae <hhh00613@naver.com>
2023-07-03 19:54:09 +0900

    project init

--- src/main/java/com/practice/lock/LockWithMySQLApplication.java
+++ src/main/java/com/practice/lock/LockWithMySQLApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LockApplication {
+public class LockWithMySQLApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LockApplication.class, args);
+        SpringApplication.run(LockWithMySQLApplication.class, args);
     }
 
 }
